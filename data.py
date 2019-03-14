e9501996c41c18e0670ce6920a6b462dabe4a02b
Oliver Barreto <oliver.barreto.online@gmail.com>
2019-03-14 19:04:23 +0000

    added new background images

---
+++ data.py
@@ -0,0 +1,466 @@
+
+def dummyArticles():
+	articles = [
+		{
+		'id' : 1,
+		'title': 'FORTNITE ITEM SHOP 10TH MARCH – NEW BANDOLETTE FORTNITE SKIN, MACHETE...',
+		'body': 'The Fortnite Item Shop has updated to display a new set of skins, pickaxes, gliders and emotes for the 10th March. The Fortntie Item...',
+		'author': '@fortniteinsider',
+		'image': 'https://fortniteinsider.com/wp-content/uploads/2019/03/Bandolette-Fortnite-Skin-533x261.jpg',
+		'source_link': 'https://fortniteinsider.com/fortnite-item-shop-10th-march-new-bandolette-fortnite-skin-machete-pickaxe-choppa-glider/',
+		'create_date': '10 marzo 2019'
+
+		}, 
+		{
+		'id' : 2,
+		'title': 'FORTNITE ITEM SHOP 9TH MARCH – ALL FORTNITE SKINS, NEW SHADOW...',
+		'body': 'The Fortnite Item Shop has updated to display a new set of skins, pickaxes, gliders and emotes for the 9th March. The item shop...',
+		'author': '@fortniteinsider',
+		'image': 'https://fortniteinsider.com/wp-content/uploads/2019/03/61e9e83817add91f2308cbfdb0becb4a-533x261.jpeg',
+		'source_link': 'https://fortniteinsider.com/fortnite-item-shop-9th-march-all-fortnite-skins-new-shadow-boxer-emote/',
+		'create_date': '10 marzo 2019'
+		},
+		{
+		'id' : 3,
+		'title': 'See how Fortnite’s Driftboard operates with this insane fan animation',
+		'body': 'Epic Games added the Driftboard vehicle into Fortnite at the beginning of Season 8, and one fan created an animation to show...',
+		'author': '@fortniteintel',
+		'image': 'https://fortniteintel.com/wp-content/uploads/2019/03/driftboard1-238x178.jpg',
+		'source_link': 'https://fortniteintel.com/see-how-fortnites-driftboard-operates-with-this-insane-fan-animation/13754/',
+		'create_date': '9 marzo 2019'
+		},
+    {
+		'id' : 4,
+		'title': 'FORTNITE SEASON 8 MAP – HERE ARE ALL THE CHANGES',
+		'body': 'Epic Games have stated they do not want to add a different map to the game just yet and are changing the existing map instead. Every season, there are new POIs (Point of Interests) to explore and there are new items and features added. The Fortnite Season 8 map has been provided by Epic Games in the Asset kit for creators, and the new map can be seen below...',
+		'author': '@fortniteinsider',
+		'image': 'https://fortniteinsider.com/wp-content/uploads/2019/02/Fortnite_patch-notes_v8-00_br-header-v8-00_08BR_POI_Volcano_Social-1920x1080-4df0b36254036724c3046e01de13b497de7349e4.jpg',
+		'source_link': 'https://fortniteinsider.com/fortnite-season-8-map-here-are-all-the-changes/',
+		'create_date': '15 marzo 2019'
+		},
+
+	]
+	return articles
+
+def Articles():
+	articles = dummyArticles()
+
+	return articles
+
+
+def dummyStore_Items():
+	items = [
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/C4A710901_large.png",
+    "manifestId": 10901,
+    "name": "Mezmer",
+    "rarity": "Sturdy",
+    "storeCategory": "BRWeeklyStorefront",
+    "vBucks": 1200
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/FD486411_large.png",
+    "manifestId": 6411,
+    "name": "Kabuto",
+    "rarity": "Sturdy",
+    "storeCategory": "BRWeeklyStorefront",
+    "vBucks": 800
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/B74C10954_large.png",
+    "manifestId": 10954,
+    "name": "Axetec",
+    "rarity": "Handmade",
+    "storeCategory": "BRWeeklyStorefront",
+    "vBucks": 800
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/D8B86399_large.png",
+    "manifestId": 6399,
+    "name": "Shogun",
+    "rarity": "Fine",
+    "storeCategory": "BRWeeklyStorefront",
+    "vBucks": 2000
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/418310902_large.png",
+    "manifestId": 10902,
+    "name": "Sunbird",
+    "rarity": "Sturdy",
+    "storeCategory": "BRWeeklyStorefront",
+    "vBucks": 1200
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/FB4410922_large.png",
+    "manifestId": 10922,
+    "name": "Sunrise",
+    "rarity": "Sturdy",
+    "storeCategory": "BRWeeklyStorefront",
+    "vBucks": 800
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/620D6415_large.png",
+    "manifestId": 6415,
+    "name": "Jawblade",
+    "rarity": "Sturdy",
+    "storeCategory": "BRWeeklyStorefront",
+    "vBucks": 800
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/9EB45593_large.png",
+    "manifestId": 5593,
+    "name": "Archetype",
+    "rarity": "Quality",
+    "storeCategory": "BRDailyStorefront",
+    "vBucks": 1500
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/5A0810915_large.png",
+    "manifestId": 10915,
+    "name": "Shadow Boxer",
+    "rarity": "Handmade",
+    "storeCategory": "BRDailyStorefront",
+    "vBucks": 200
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/459E6367_large.png",
+    "manifestId": 6367,
+    "name": "Criss Cross",
+    "rarity": "Sturdy",
+    "storeCategory": "BRDailyStorefront",
+    "vBucks": 500
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/6C01717_large.png",
+    "manifestId": 717,
+    "name": "Batsickle",
+    "rarity": "Sturdy",
+    "storeCategory": "BRDailyStorefront",
+    "vBucks": 800
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/98D46155_large.png",
+    "manifestId": 6155,
+    "name": "Grill Sergeant",
+    "rarity": "Handmade",
+    "storeCategory": "BRDailyStorefront",
+    "vBucks": 800
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/4DFB6408_large.png",
+    "manifestId": 6408,
+    "name": "Junkjet",
+    "rarity": "Quality",
+    "storeCategory": "BRDailyStorefront",
+    "vBucks": 1200
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/AEBF10896_large.png",
+    "manifestId": 10896,
+    "name": "Bandolette",
+    "rarity": "Sturdy",
+    "storeCategory": "BRWeeklyStorefront",
+    "vBucks": 1200
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/B06C10952_large.png",
+    "manifestId": 10952,
+    "name": "Machete",
+    "rarity": "Handmade",
+    "storeCategory": "BRWeeklyStorefront",
+    "vBucks": 500
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/B19C10921_large.png",
+    "manifestId": 10921,
+    "name": "Choppa",
+    "rarity": "Quality",
+    "storeCategory": "BRWeeklyStorefront",
+    "vBucks": 1200
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/54486112_large.png",
+    "manifestId": 6112,
+    "name": "Field Surgeon",
+    "rarity": "Quality",
+    "storeCategory": "BRDailyStorefront",
+    "vBucks": 1500
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/AC746010_large.png",
+    "manifestId": 6010,
+    "name": "Armadillo",
+    "rarity": "Handmade",
+    "storeCategory": "BRDailyStorefront",
+    "vBucks": 800
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/2108882_large.png",
+    "manifestId": 882,
+    "name": "Warthog",
+    "rarity": "Handmade",
+    "storeCategory": "BRDailyStorefront",
+    "vBucks": 500
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/89FB5592_large.png",
+    "manifestId": 5592,
+    "name": "Clutch Axe",
+    "rarity": "Sturdy",
+    "storeCategory": "BRDailyStorefront",
+    "vBucks": 800
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/96136479_large.png",
+    "manifestId": 6479,
+    "name": "Knee Slapper",
+    "rarity": "Sturdy",
+    "storeCategory": "BRDailyStorefront",
+    "vBucks": 500
+  },
+  {
+    "imageUrl": "https://cdn.thetrackernetwork.com/cdn/fortnite/714B5726_large.png",
+    "manifestId": 5726,
+    "name": "Hand Signals",
+    "rarity": "Sturdy",
+    "storeCategory": "BRDailyStorefront",
+    "vBucks": 500
+  }
+]
+	return items
+
+
+def StoreItems():
+	store_items = dummyStore_Items()
+
+	return store_items
+
+def dummyChallenges():
+	challenges = {
+  "items": [
+    {
+      "metadata": [
+        {
+          "key": "type",
+          "value": "challenge"
+        },
+        {
+          "key": "name",
+          "value": "Get 30s of airtime in a vehicle"
+        },
+        {
+          "key": "questsCompleted",
+          "value": "0"
+        },
+        {
+          "key": "questsTotal",
+          "value": "30"
+        },
+        {
+          "key": "rewardPictureUrl",
+          "value": "https://cdn.thetrackernetwork.com/cdn/trackernetwork/63D2upload.png"
+        },
+        {
+          "key": "rewardName",
+          "value": "5"
+        },
+        {
+          "key": "rewardDescription"
+        }
+      ]
+    },
+    {
+      "metadata": [
+        {
+          "key": "type",
+          "value": "challenge"
+        },
+        {
+          "key": "name",
+          "value": "Deal damage with Shotguns to opponents"
+        },
+        {
+          "key": "questsCompleted",
+          "value": "0"
+        },
+        {
+          "key": "questsTotal",
+          "value": "1000"
+        },
+        {
+          "key": "rewardPictureUrl",
+          "value": "https://cdn.thetrackernetwork.com/cdn/trackernetwork/63D2upload.png"
+        },
+        {
+          "key": "rewardName",
+          "value": "5"
+        },
+        {
+          "key": "rewardDescription"
+        }
+      ]
+    },
+    {
+      "metadata": [
+        {
+          "key": "type",
+          "value": "challenge"
+        },
+        {
+          "key": "name",
+          "value": "Stage 1: Consume Mushrooms"
+        },
+        {
+          "key": "questsCompleted",
+          "value": "0"
+        },
+        {
+          "key": "questsTotal",
+          "value": "5"
+        },
+        {
+          "key": "rewardPictureUrl",
+          "value": "https://cdn.thetrackernetwork.com/cdn/trackernetwork/63D2upload.png"
+        },
+        {
+          "key": "rewardName",
+          "value": "2"
+        },
+        {
+          "key": "rewardDescription"
+        }
+      ]
+    },
+    {
+      "metadata": [
+        {
+          "key": "type",
+          "value": "challenge"
+        },
+        {
+          "key": "name",
+          "value": "Deal damage with Clingers, Grenades, or Stink Bombs to opponents"
+        },
+        {
+          "key": "questsCompleted",
+          "value": "0"
+        },
+        {
+          "key": "questsTotal",
+          "value": "500"
+        },
+        {
+          "key": "rewardPictureUrl",
+          "value": "https://cdn.thetrackernetwork.com/cdn/trackernetwork/63D2upload.png"
+        },
+        {
+          "key": "rewardName",
+          "value": "5"
+        },
+        {
+          "key": "rewardDescription"
+        }
+      ]
+    },
+    {
+      "metadata": [
+        {
+          "key": "type",
+          "value": "challenge"
+        },
+        {
+          "key": "name",
+          "value": "Deal damage to opponent structures with Dynamite"
+        },
+        {
+          "key": "questsCompleted",
+          "value": "0"
+        },
+        {
+          "key": "questsTotal",
+          "value": "10000"
+        },
+        {
+          "key": "rewardPictureUrl",
+          "value": "https://cdn.thetrackernetwork.com/cdn/trackernetwork/63D2upload.png"
+        },
+        {
+          "key": "rewardName",
+          "value": "5"
+        },
+        {
+          "key": "rewardDescription"
+        }
+      ]
+    },
+    {
+      "metadata": [
+        {
+          "key": "type",
+          "value": "challenge"
+        },
+        {
+          "key": "name",
+          "value": "Rocket or Grenade Launcher Eliminations"
+        },
+        {
+          "key": "questsCompleted",
+          "value": "0"
+        },
+        {
+          "key": "questsTotal",
+          "value": "3"
+        },
+        {
+          "key": "rewardPictureUrl",
+          "value": "https://cdn.thetrackernetwork.com/cdn/trackernetwork/63D2upload.png"
+        },
+        {
+          "key": "rewardName",
+          "value": "10"
+        },
+        {
+          "key": "rewardDescription"
+        }
+      ]
+    },
+    {
+      "metadata": [
+        {
+          "key": "type",
+          "value": "challenge"
+        },
+        {
+          "key": "name",
+          "value": "Stage 1: Deal damage with Grenades to opponents"
+        },
+        {
+          "key": "questsCompleted",
+          "value": "0"
+        },
+        {
+          "key": "questsTotal",
+          "value": "100"
+        },
+        {
+          "key": "rewardPictureUrl",
+          "value": "https://cdn.thetrackernetwork.com/cdn/trackernetwork/63D2upload.png"
+        },
+        {
+          "key": "rewardName",
+          "value": "3"
+        },
+        {
+          "key": "rewardDescription"
+        }
+      ]
+    }
+  ]
+}
+
+def Challenges():
+	challenges = dummyChallenges()
+
+	return challenges
+
+
